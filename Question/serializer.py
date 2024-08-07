0c37f8058926a9a7604597ea82da520a696719a8
tr1b1iwnl <tr1b1iwnl@gmail.com>
2024-08-07 12:13:40 +0800

    Added attachments to question creation

--- Question/serializer.py
+++ Question/serializer.py
@@ -21,39 +21,15 @@ class QuestionSerializer(serializers.ModelSerializer):
         fields = ['id', 'lesson', 'text', 'difficulty', 'choices', 'attachments']
 
     def create(self, validated_data):
-        choices_data = validated_data.pop('choices', [])
+        choices_data = validated_data.pop('choices')
         attachments_data = validated_data.pop('attachments', [])
         question = Question.objects.create(**validated_data)
-
         for choice_data in choices_data:
             Choice.objects.create(question=question, **choice_data)
-
         for attachment_data in attachments_data:
             attachment = Attachment.objects.create(**attachment_data)
             question.attachments.add(attachment)
-
         return question
-
-    def update(self, instance, validated_data):
-        choices_data = validated_data.pop('choices', [])
-        attachments_data = validated_data.pop('attachments', [])
-
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save()
-
-        if choices_data:
-            instance.choices.all().delete()
-            for choice_data in choices_data:
-                Choice.objects.create(question=instance, **choice_data)
-
-        if attachments_data:
-            instance.attachments.clear()
-            for attachment_data in attachments_data:
-                attachment = Attachment.objects.create(**attachment_data)
-                instance.attachments.add(attachment)
-
-        return instance
 
 
 class StudentAnswerSerializer(serializers.ModelSerializer):
