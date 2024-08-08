93da4d82a9ce0c796fe95b177ac0bfc35ef2dc41
Jomar Magdalaga <jmagdalaga5@gmail.com>
2024-08-08 23:15:20 +0800

    update question on serializer

--- Question/serializer.py
+++ Question/serializer.py
@@ -1,16 +1,13 @@
 from rest_framework import serializers
-
 from Class.models import Attachment
 from Class.serializers import AttachmentSerializer
 from Course.models import Lesson
 from .models import Question, Choice, StudentAnswer
 
-
 class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields = ['id', 'text', 'is_correct']
-
 
 class QuestionSerializer(serializers.ModelSerializer):
     choices = ChoiceSerializer(many=True)
@@ -29,6 +26,41 @@ class QuestionSerializer(serializers.ModelSerializer):
         for attachment in attachments_data:
             question.attachments.add(attachment)
         return question
+
+    def update(self, instance, validated_data):
+        choices_data = validated_data.pop('choices', None)
+        attachments_data = validated_data.pop('attachments', None)
+
+        instance.lesson = validated_data.get('lesson', instance.lesson)
+        instance.text = validated_data.get('text', instance.text)
+        instance.difficulty = validated_data.get('difficulty', instance.difficulty)
+        instance.save()
+
+        if choices_data is not None:
+            existing_choice_ids = {choice.id for choice in instance.choices.all()}
+            new_choice_ids = set()
+
+            for choice_data in choices_data:
+                choice_id = choice_data.get('id')
+                if choice_id and choice_id in existing_choice_ids:
+                    choice = Choice.objects.get(id=choice_id, question=instance)
+                    choice.text = choice_data.get('text', choice.text)
+                    choice.is_correct = choice_data.get('is_correct', choice.is_correct)
+                    choice.save()
+                    new_choice_ids.add(choice_id)
+                elif not choice_id:
+                    new_choice = Choice.objects.create(question=instance, **choice_data)
+                    new_choice_ids.add(new_choice.id)
+
+            for choice_id in existing_choice_ids - new_choice_ids:
+                Choice.objects.filter(id=choice_id, question=instance).delete()
+
+        if attachments_data is not None:
+            instance.attachments.clear()
+            for attachment in attachments_data:
+                instance.attachments.add(attachment)
+
+        return instance
 
 
 class StudentAnswerSerializer(serializers.ModelSerializer):
