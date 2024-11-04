1cd84e54d745101246d0de7c23fabc967e17c502
kenjiebalona <kbalona5@gmail.com>
2024-11-05 05:35:06 +0800

    fix: Exam (lesson to subtopic)

--- Exam/views.py
+++ Exam/views.py
@@ -185,7 +185,7 @@ class ExamViewSet(viewsets.ModelViewSet):
             attempt.failed_lessons.set(failed_subtopic_ids)
             StudentLessonProgress.objects.filter(
                 student=student,
-                lesson__in=failed_subtopics_id
+                lesson__in=failed_subtopic_ids
             ).update(is_completed=False)
             
             print(f"Failed subtopics set: {failed_subtopics}")  # Debug print
