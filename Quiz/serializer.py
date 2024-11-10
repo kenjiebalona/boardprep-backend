5cc8a353f4c30d8fb22cf21ae4b8b7421427d7d7
kenjiebalona <kbalona5@gmail.com>
2024-11-11 00:45:08 +0800

    chore: update quiz model

--- Quiz/serializer.py
+++ Quiz/serializer.py
@@ -9,7 +9,7 @@ class QuizSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Quiz
-        fields = ['id', 'student', 'class_instance', 'subtopic', 'title', 'questions']
+        fields = ['id', 'student', 'class_instance', 'learning_objective', 'title', 'questions']
 
 
 class StudentQuizAttemptSerializer(serializers.ModelSerializer):
